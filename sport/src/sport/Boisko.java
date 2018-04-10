c77412b46ef040ea7fe1e511c7f66aa4e68bf824
Miłosz <milosz.gus@gmail.com>
2018-04-10 17:36:24 +0200

    Add files via upload
    package sport do importu enum'a materialy

---
+++ sport/src/sport/Boisko.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sport;
+
+public class Boisko {
+
+   public enum materialy{guma, skóra, guwno, kurwa, szmata}
+    public static void main(String[] args) {
+
+        
+        
+    }
+    
+}
